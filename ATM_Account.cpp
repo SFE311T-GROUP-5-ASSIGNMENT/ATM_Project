843e9d0a97b93f7c11069052c6338378c661f432
S.Mvelase <samkelo10@gmail.com>
2021-07-11 11:50:30 +0200

    fixed build errors

--- ATM_Account.cpp
+++ ATM_Account.cpp
@@ -2,10 +2,33 @@
 #include "CheckingAccount.h"
 #include "SavingsAccount.h"
 
+using namespace std;
+int option;
+void menu(int&);
 int main()
 {
+	
+	menu(option);
 
-
+	cout <<option << endl;
+	//cout << option<<endl;
 
 	return 0;
+}
+
+void menu(int&option)
+{
+	
+	cout << "\n\t*************MENU****************";
+	cout << "\n\t*                               *";
+	cout << "\n\t*        1. Check Balance       *";
+	cout << "\n\t*        2. Deposit             *";
+	cout << "\n\t*        3. Withdraw            *";
+	cout << "\n\t*        4. Transfer            *";
+	cout << "\n\t*        5. Exit                *";
+	cout << "\n\t*                               *";
+	cout << "\n\t*********************************";
+	cout << "\n\t  Please choose an option: ";
+	cin >> option;
+	
 }
